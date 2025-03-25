4a814c9a1a8c926d17cc40112c7f290061d71998
Francisco Rocha <fa676533@gmail.com>
2025-03-24 18:28:27 -0600

    connected database, created startup section and database deployment

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,10 +1,25 @@
 import { Viaje } from "../models/Viaje.js";
+import { Testimonial } from "../models/Testimoniales.js";
 
-const paginaInicio = (req, res) => {
-  // req - lo que enviamos : res - los que express nos responde
-  res.render("inicio", {
-    pagina: "Inicio",
-  });
+const paginaInicio = async (req, res) => {
+  // CONSULTAR 3 VIAJES DEL MODELO VIAJE
+
+  const promiseDB = [];
+
+  promiseDB.push(Viaje.findAll({ limit: 3 }));
+  promiseDB.push(Testimonial.findAll({ limit: 4 }));
+  try {
+    const resultado = await Promise.all(promiseDB);
+    // req - lo que enviamos : res - los que express nos responde
+    res.render("inicio", {
+      pagina: "Inicio",
+      clase: "home",
+      viajes: resultado[0],
+      testimoniales: resultado[1],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const paginaNosotros = (req, res) => {
@@ -24,10 +39,16 @@ const paginaViajes = async (req, res) => {
   });
 };
 
-const paginaTestimoniales = (req, res) => {
-  res.render("testimoniales", {
-    pagina: "Testimoniales",
-  });
+const paginaTestimoniales = async (req, res) => {
+  try {
+    const testimoniales = await Testimonial.findAll();
+    res.render("testimoniales", {
+      pagina: "Testimoniales",
+      testimoniales,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //Muestra un viaje por su slug
